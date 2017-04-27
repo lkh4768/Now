5e822dba981d0cfa6f886448a3185ff5ee910a97
wes <lkh5510@gmail.com>
2017-04-28 00:18:00 +0900

    update ServerInitializer

--- src/main/java/ServerInitializer.java
+++ src/main/java/ServerInitializer.java
@@ -1,5 +1,12 @@
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.cors.CorsConfig;
+import io.netty.handler.codec.http.cors.CorsHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * Created by WES on 2017-04-27.
@@ -7,6 +14,10 @@ import io.netty.channel.socket.SocketChannel;
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpResponseEncoder());
+        pipeline.addLast(new HttpRequestDecoder());
+        pipeline.addLast(new HttpObjectAggregator(65536));
+        pipeline.addLast(new ChunkedWriteHandler());
     }
 }
