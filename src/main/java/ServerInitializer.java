684ef64958b246fab90786b56af5e9121a5bf9f9
wes <lkh5510@gmail.com>
2017-04-29 13:30:57 +0900

    Update ServerInitializer - update codec and handler

--- src/main/java/ServerInitializer.java
+++ src/main/java/ServerInitializer.java
@@ -1,12 +1,7 @@
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
-import io.netty.handler.codec.http.cors.CorsConfig;
-import io.netty.handler.codec.http.cors.CorsHandler;
-import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.codec.http.HttpServerCodec;
 
 /**
  * Created by WES on 2017-04-27.
@@ -14,10 +9,8 @@ import io.netty.handler.stream.ChunkedWriteHandler;
 public class ServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-        ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast(new HttpResponseEncoder());
-        pipeline.addLast(new HttpRequestDecoder());
-        pipeline.addLast(new HttpObjectAggregator(65536));
-        pipeline.addLast(new ChunkedWriteHandler());
+        ChannelPipeline p = ch.pipeline();
+        p.addLast(new HttpServerCodec());
+        /* custom handler */
     }
 }
